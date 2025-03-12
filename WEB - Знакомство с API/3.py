2f417cf04215907418c6c5247d454dbe64f949d9
Tereegor <tereschenko.er@mail.ru>
2025-03-12 22:25:37 +0300

    Области

---
+++ WEB - Знакомство с API/3.py
@@ -0,0 +1,16 @@
+import requests
+
+api_key = "8013b162-6b42-4997-9691-77b7074026e0"
+cities = ["Барнаул", "Мелеуз", "Йошкар-Ола"]
+
+for city in cities:
+    url = f"http://geocode-maps.yandex.ru/1.x/?apikey={api_key}&geocode={city}&format=json"
+    response = requests.get(url).json()
+    toponym = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    metadata = toponym["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"]
+
+    region = next((comp["name"] for comp in metadata if comp["kind"] == "province"), "Не указан")
+
+    print(f"Город: {city}")
+    print(f"Область: {region}")
+    print("-" * 30)
