72c71a1cb1bf8cb70488b92bcb2e765b23ff45a2
Tereegor <tereschenko.er@mail.ru>
2025-03-15 23:05:34 +0300

    Варианты выбора

--- WEB - Введение во flask/6/main.py
+++ WEB - Введение во flask/6/main.py
@@ -2,19 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-colors = ["secondary", "success", "secondary", "warning", "danger"]
-
 
 @app.route("/choice/<planet_name>")
 def choice(planet_name):
-    if planet_name.lower() == "mars":
-        return render_template("mars.html", planet_name=planet_name, colors=colors)
-    elif planet_name.lower() == "venus":
-        return render_template("venus.html", planet_name=planet_name, colors=colors)
-    elif planet_name.lower() == "jupiter":
-        return render_template("jupiter.html", planet_name=planet_name, colors=colors)
-    else:
-        return "Планета не найдена", 404
+    return render_template("choice.html", planet_name=planet_name, title="Выбор планеты")
 
 
 if __name__ == "__main__":
